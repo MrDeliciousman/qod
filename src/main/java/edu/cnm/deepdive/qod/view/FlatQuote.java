941fac4fd2aaf73a16c8261783e456409f7bee0c
Thomas Herrera <rayherrera90@outlook.com>
2019-04-01 15:14:09 -0600

    Completed the many-to-many implementation.

---
+++ src/main/java/edu/cnm/deepdive/qod/view/FlatQuote.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.qod.view;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.UUID;
+
+public interface FlatQuote {
+
+  UUID getId();
+
+  Date getCreated();
+
+  String getText();
+
+  URI getHref();
+}
---
+++ src/main/java/edu/cnm/deepdive/qod/view/FlatSource.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.qod.view;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.UUID;
+
+public interface FlatSource {
+
+  UUID getId();
+
+  Date getCreated();
+
+  String getName();
+
+  URI getHref();
+
+}
